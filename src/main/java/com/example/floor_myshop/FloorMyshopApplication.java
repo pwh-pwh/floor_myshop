ef79b3d39b2d13dd61abb4b5a18ecec6a80c10cd
pwh-pwh <plmoknpwh@163.com>
2021-12-19 11:21:43 +0800

    first commit

---
+++ src/main/java/com/example/floor_myshop/FloorMyshopApplication.java
@@ -0,0 +1,13 @@
+package com.example.floor_myshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloorMyshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FloorMyshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/floor_myshop/FloorMyshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.floor_myshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloorMyshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
