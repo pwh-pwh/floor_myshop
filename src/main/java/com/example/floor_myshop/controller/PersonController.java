8667dcc54421022d4889b1f1bc60756bff4ffa29
pwh-pwh <plmoknpwh@163.com>
2021-12-27 21:42:27 +0800

    获取用户信息及更新

--- src/main/java/com/example/floor_myshop/controller/PersonController.java
+++ src/main/java/com/example/floor_myshop/controller/PersonController.java
@@ -1,8 +1,12 @@
 package com.example.floor_myshop.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.floor_myshop.entity.Person;
+import com.example.floor_myshop.model.ApiResponse;
+import com.example.floor_myshop.service.IPersonService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -15,8 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/person")
 public class PersonController {
+    @Autowired
+    IPersonService personService;
 
-
+    @GetMapping("info")
+    public ApiResponse info(@RequestParam Integer id) {
+        Person byId = personService.getById(id);
+        return ApiResponse.success(byId);
+    }
+    @PostMapping("update")
+    public ApiResponse update(@RequestBody Person p ) {
+        personService.updateById(p);
+        return ApiResponse.success("update success");
+    }
 
 
 }
